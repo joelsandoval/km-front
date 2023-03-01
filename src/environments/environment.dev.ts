02e70ffff527d655cebf33b2f04e61b617ace4ea
Joel Sandoval <desarrollo@semarnat.gob.mx>
2023-03-01 11:54:05 -0600

    km

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
   ApiConfig: {
-    rutaBase:   'http://localhost:8087/operacion/',
+    rutaBase:   'http://e-scan.ovh:8087/operacion/',
     rutaIssuer: 'http://e-scan.ovh:8180/realms/kmc'
   }
 
