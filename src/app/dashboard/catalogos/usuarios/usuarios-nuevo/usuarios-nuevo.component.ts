36d13a45f699ccd1ea3ccaed04ade8c883842ec9
gonzalo <gonzalo@mail.com>
2023-03-08 16:05:47 -0600

    Se añade la funcionalidad para crear usuarios

--- src/app/dashboard/catalogos/usuarios/usuarios-nuevo/usuarios-nuevo.component.ts
+++ src/app/dashboard/catalogos/usuarios/usuarios-nuevo/usuarios-nuevo.component.ts
@@ -13,42 +13,43 @@ import { Router } from '@angular/router';
 })
 export class UsuariosNuevoComponent implements OnInit {
 
-  username: string ='';
-   email: string ='';
-   firstName: string ='';
-   lastName: string ='';
-   password: string ='';
+  username: string = '';
+  email: string = '';
+  firstName: string = '';
+  lastName: string = '';
+  password: string = '';
   usuario: User = new User(this.username, this.email, this.firstName, this.lastName, this.password);
 
 
   constructor(
     public dialogRef: MatDialogRef<UsuariosNuevoComponent>,
+    public dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: number,
     private serviceUser: UserService,
     private _snackBar: MatSnackBar,
     private router: Router
-    ) { }
+  ) { }
 
   ngOnInit() {
   }
 
-  guardaUser(){
-    this.usuario.username= this.usuario.username;
-    this.usuario.firstName=this.usuario.firstName;
-    this.usuario.lastName=this.usuario.lastName;
-    this.usuario.email=this.usuario.email;
-    this.usuario.password=this.usuario.password;
+  guardaUser() {
+    this.usuario.username = this.usuario.username;
+    this.usuario.firstName = this.usuario.firstName;
+    this.usuario.lastName = this.usuario.lastName;
+    this.usuario.email = this.usuario.email;
+    this.usuario.password = this.usuario.password;
     this.serviceUser.create(this.usuario).subscribe(
       res => {
         this.openSnackBar('El usuario se ha guardado con éxito', 'ok');
-        this.router.navigate(['catalogos']);
+        this.router.navigate(['../usuarios']);
       }
     )
   }
 
   openSnackBar(message: string, action: string) {
-  this._snackBar.open(message, action);
-}
+    this._snackBar.open(message, action);
+  }
 
 
 }
